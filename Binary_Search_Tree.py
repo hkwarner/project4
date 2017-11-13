222510a0dae6502299f1daf1b6822f4daa46d45e
Hana <hkwarner@email.wm.edu>
2017-11-13 09:51:27 -0500

    height for insert
    need to check, removal should be reverse

--- Binary_Search_Tree.py
+++ Binary_Search_Tree.py
@@ -12,6 +12,7 @@ class Binary_Search_Tree:
 
     def __init__(self):
         self.__root = None
+        self.__height = 0
 
     def rec_insert(self, value, cur_node): # should be correct
         if cur_node == None:
@@ -19,19 +20,21 @@ class Binary_Search_Tree:
         elif cur_node != None:
             if value < cur_node.value:
                 cur_node.left = self.rec_insert(value, cur_node.left)
+                cur_node.height = cur_node.left.height + 1
             elif value > cur_node.value:
                 cur_node.right = self.rec_insert(value, cur_node.right)
+                cur_node.height = cur_node.right.height + 1
             elif value == cur_node.value:
                 raise ValueError
+            if self.__height < self.__cur_node.height:
+                self.__height = cur_node.height
             return cur_node
 
     def insert_element(self, value):
         self.__root = self.rec_insert(value, self.__root)
 
     def remove_element(self, value):
-        if self.__root == None: print('root is None before removal')
         self.__root = self.__rec_remove(value, self.__root)
-        if self.__root == None: print('root is None') #self.__root is being returned as none-->not assigned
 
     def __rec_remove(self, value, cur_node):
         if cur_node == None:
@@ -40,14 +43,14 @@ class Binary_Search_Tree:
             cur_node.left = self.__rec_remove(value, cur_node.left)
         elif value > cur_node.value:
             cur_node.right = self.__rec_remove(value, cur_node.right)
-        elif value == cur_node.value: # value found
+        elif value == cur_node.value:
             if cur_node.left != None and cur_node.right != None:
                 prec = cur_node
                 replace_with = cur.right
                 while replace_with.left != None:
                     prec = replace_with
                     replace_with = replace_with.left
-                current.val = replace_with.val #this should probably return something
+                current.val = replace_with.val
                 prec.left = None
             elif cur_node.right == None:
                 cur_node = cur_node.left
@@ -126,10 +129,7 @@ class Binary_Search_Tree:
         pass # TODO replace pass with your implementation
 
     def get_height(self):
-    # return an integer that represents the height of the tree.
-    # assume that an empty tree has height 0 and a tree with one
-    # node has height 1. This method must operate in constant time.
-        pass # TODO replace pass with your implementation
+        return self.__height
 
     def __str__(self):
         return self.in_order()
